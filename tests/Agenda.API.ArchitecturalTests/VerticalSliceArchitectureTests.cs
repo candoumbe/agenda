b4da7f5669dbe3f88ed7b580cd6400ad5231eab5
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-08-24 18:15:33 +0200

    feat: update .NET target frameworks
    - Update Agenda.API.IntegrationTests to net9.0
    - Update Agenda.Ids to netstandard2.1
    - Update Agenda.Ids.UnitTests to net8.0 and net9.0
    - Update Agenda.DataStores to net9.0
    - Update Agenda.Objects to netstandard2.1, net8.0, and net9.0
    - Update Agenda.Pipelines to net9.0
    - Update Agenda.DataStores.Postgres to net9.0
    - Update Agenda.DataStores.Sqlite to net9.0
    - Update Agenda.API to net9.0
    - Update Agenda.API.ArchitecturalTests to net8.0 and net9.0
    - Update Agenda.API.UnitTests to net9.0
    - Add conditional compilation for .NET 9.0 in VerticalSliceArchitectureTests.cs

--- tests/Agenda.API.ArchitecturalTests/VerticalSliceArchitectureTests.cs
+++ tests/Agenda.API.ArchitecturalTests/VerticalSliceArchitectureTests.cs
@@ -1,3 +1,4 @@
+#if NET9_0_OR_GREATER
 using System.Collections.Generic;
 using System.Linq;
 using ArchUnitNET.Domain;
@@ -13,76 +14,78 @@ using Xunit.Categories;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 using Assembly = System.Reflection.Assembly;
 
-namespace Agenda.API.ArchitecturalTests;
-
-[UnitTest]
-public class VerticalSliceArchitectureTests
+namespace Agenda.API.ArchitecturalTests
 {
-    private static readonly Assembly s_apiAssembly = typeof(Program).Assembly;
-    private static readonly Architecture s_apiArchitecture = new ArchLoader().LoadAssemblies(s_apiAssembly).Build();
-
-    private static readonly IType s_endpointType = s_apiArchitecture.GetITypeOfType(typeof(IEndpoint));
-
-    private static readonly IType s_endpointWithRequestAndResponse = s_apiArchitecture.GetITypeOfType(typeof(Endpoint<,>));
-    private static readonly IType s_endpointWithRequestOnly = s_apiArchitecture.GetITypeOfType(typeof(Endpoint<>));
-
-    private static GivenClassesConjunction EndpointsWithRequest => Endpoints
-        .And().AreAssignableTo(s_endpointWithRequestAndResponse);
-
-    private static IObjectProvider<Class> EndpointsWithRequestAndResponse => Endpoints
-        .And().AreAssignableTo(s_endpointWithRequestAndResponse)
-        .And().AreNotAssignableTo(s_endpointWithRequestOnly);
-
-
-    private static GivenClassesConjunctionWithDescription Endpoints => Classes().That().ResideInAssembly(s_apiAssembly)
-        .And().AreNotAbstract()
-        .And().AreAssignableTo(typeof(IEndpoint))
-        .As("Endpoints");
-
-
-    [Fact]
-    public void Endpoints_should_be_in_vertical_slice_architecture()
+    [UnitTest]
+    public class VerticalSliceArchitectureTests
     {
-        IArchRule endpointsResideInResourceNamespace = Endpoints
-            .Should().ResideInNamespaceMatching(@"Agenda.API.Resources.*")
-            .Because("Endpoints should be organized by feature (vertical slice) instead of technical details");
+        private static readonly Assembly s_apiAssembly = typeof(Program).Assembly;
+        private static readonly Architecture s_apiArchitecture = new ArchLoader().LoadAssemblies(s_apiAssembly).Build();
 
-        IArchRule endpointsResideInItsOwnNamespace = Endpoints
-            .Should()
-            .FollowCustomCondition(endpoint =>
-                                   {
-                                       IEnumerable<Class> otherEndpoints = endpoint.Namespace.Classes.Where(c => !Equals(c, endpoint)
-                                                                                                              && c.IsAssignableTo(s_endpointType));
+        private static readonly IType s_endpointType = s_apiArchitecture.GetITypeOfType(typeof(IEndpoint));
 
-                                       return new ConditionResult(endpoint,
-                                                                  !otherEndpoints.Any(),
-                                                                  "should be in its own namespace");
-                                   },
-                                   "be in its own namespace");
+        private static readonly IType s_endpointWithRequestAndResponse = s_apiArchitecture.GetITypeOfType(typeof(Endpoint<,>));
+        private static readonly IType s_endpointWithRequestOnly = s_apiArchitecture.GetITypeOfType(typeof(Endpoint<>));
 
-        endpointsResideInResourceNamespace
-            .And(endpointsResideInItsOwnNamespace)
-            .Check(s_apiArchitecture);
+        private static GivenClassesConjunction EndpointsWithRequest => Endpoints
+            .And().AreAssignableTo(s_endpointWithRequestAndResponse);
 
-    }
+        private static IObjectProvider<Class> EndpointsWithRequestAndResponse => Endpoints
+            .And().AreAssignableTo(s_endpointWithRequestAndResponse)
+            .And().AreNotAssignableTo(s_endpointWithRequestOnly);
 
-    [Fact]
-    public void Endpoint_should_reside_in_the_same_namespace_as_its_request()
-    {
-        IArchRule endpointResideInSameNamespaceAsRequest = EndpointsWithRequest
-            .Should()
-            .FollowCustomCondition(endpoint =>
-                                   {
-                                       GenericArgument request = endpoint.GetInheritsBaseClassDependencies().First().TargetGenericArguments.First();
-                                       IType requestType = request.Type;
-                                       Namespace requestNamespace = requestType.Namespace;
-                                       bool requestNamespaceIsSameAsEndpointNamespace = requestNamespace.Equals(endpoint.Namespace);
-                                       return new ConditionResult(endpoint,
-                                                                  requestNamespaceIsSameAsEndpointNamespace,
-                                                                  $"should not use request type reside in the same '{endpoint.Namespace}' namespace ('{requestType.Name}' is in '{requestNamespace}')");
-                                   },
-                                   "reside in the same namespace as its request");
 
-        endpointResideInSameNamespaceAsRequest.Check(s_apiArchitecture);
+        private static GivenClassesConjunctionWithDescription Endpoints => Classes().That().ResideInAssembly(s_apiAssembly)
+            .And().AreNotAbstract()
+            .And().AreAssignableTo(typeof(IEndpoint))
+            .As("Endpoints");
+
+
+        [Fact]
+        public void Endpoints_should_be_in_vertical_slice_architecture()
+        {
+            IArchRule endpointsResideInResourceNamespace = Endpoints
+                .Should().ResideInNamespaceMatching(@"Agenda.API.Resources.*")
+                .Because("Endpoints should be organized by feature (vertical slice) instead of technical details");
+
+            IArchRule endpointsResideInItsOwnNamespace = Endpoints
+                .Should()
+                .FollowCustomCondition(endpoint =>
+                                       {
+                                           IEnumerable<Class> otherEndpoints = endpoint.Namespace.Classes.Where(c => !Equals(c, endpoint)
+                                                                                                                     && c.IsAssignableTo(s_endpointType));
+
+                                           return new ConditionResult(endpoint,
+                                                                      !otherEndpoints.Any(),
+                                                                      "should be in its own namespace");
+                                       },
+                                       "be in its own namespace");
+
+            endpointsResideInResourceNamespace
+                .And(endpointsResideInItsOwnNamespace)
+                .Check(s_apiArchitecture);
+
+        }
+
+        [Fact]
+        public void Endpoint_should_reside_in_the_same_namespace_as_its_request()
+        {
+            IArchRule endpointResideInSameNamespaceAsRequest = EndpointsWithRequest
+                .Should()
+                .FollowCustomCondition(endpoint =>
+                                       {
+                                           GenericArgument request = endpoint.GetInheritsBaseClassDependencies().First().TargetGenericArguments.First();
+                                           IType requestType = request.Type;
+                                           Namespace requestNamespace = requestType.Namespace;
+                                           bool requestNamespaceIsSameAsEndpointNamespace = requestNamespace.Equals(endpoint.Namespace);
+                                           return new ConditionResult(endpoint,
+                                                                      requestNamespaceIsSameAsEndpointNamespace,
+                                                                      $"should not use request type reside in the same '{endpoint.Namespace}' namespace ('{requestType.Name}' is in '{requestNamespace}')");
+                                       },
+                                       "reside in the same namespace as its request");
+
+            endpointResideInSameNamespaceAsRequest.Check(s_apiArchitecture);
+        }
     }
 }
+#endif
