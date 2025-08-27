6c965dbbcbf6030e2d399169227dca9f1c5e971b
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-08-27 02:14:05 +0200

    chore: add documentation

--- src/Agenda.API/Resources/Appointments/v1/Create/CreateAppointmentSerializerContext.cs
+++ src/Agenda.API/Resources/Appointments/v1/Create/CreateAppointmentSerializerContext.cs
@@ -1,13 +1,15 @@
 using System.Text.Json.Serialization;
 using FastEndpoints;
 
-namespace Agenda.API.Resources.Appointments.v1.Create
-{
-        [JsonSerializable(typeof(NewAppointmentInfo))]
-        [JsonSerializable(typeof(Browsable<AppointmentInfo>))]
-        [JsonSerializable(typeof(ProblemDetails))]
-        public partial class CreateAppointmentSerializerContext : JsonSerializerContext
-        {
+namespace Agenda.API.Resources.Appointments.v1.Create;
 
-        }
+/// <summary>
+/// Source generated serializer context for <see cref="NewAppointmentInfo"/>, <see cref="Browsable{T}"/> and <see cref="ProblemDetails"/>.
+/// </summary>
+[JsonSerializable(typeof(NewAppointmentInfo))]
+[JsonSerializable(typeof(Browsable<AppointmentInfo>))]
+[JsonSerializable(typeof(ProblemDetails))]
+public partial class CreateAppointmentSerializerContext : JsonSerializerContext
+{
+
 }
