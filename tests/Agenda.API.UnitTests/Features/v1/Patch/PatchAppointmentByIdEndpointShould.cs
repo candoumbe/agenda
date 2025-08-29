d6a76489a3441a4d7041d9d940356a62c441f547
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-08-29 21:30:44 +0200

    feat(tests): add unit tests for `PatchAppointmentByIdEndpoint`
    - Add unit tests for PatchAppointmentByIdEndpoint
      - Verify expected route and HTTP method
      - Ensure NoContent is returned

---
+++ tests/Agenda.API.UnitTests/Features/v1/Patch/PatchAppointmentByIdEndpointShould.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Agenda.API.Resources.Appointments.v1.Update;
+using FastEndpoints;
+using FluentAssertions;
+using Xunit;
+
+namespace Agenda.API.UnitTests.Features.v1.Patch;
+
+public class PatchAppointmentByIdEndpointShould
+{
+    private readonly PatchAppointmentByIdEndpoint _sut = Factory.Create<PatchAppointmentByIdEndpoint>();
+
+    [Fact]
+    public void Have_expected_route()
+    {
+        // Assert
+        string[] routes = _sut.Definition.Routes;
+        routes.Should()
+            .HaveCount(1)
+            .And
+            .ContainSingle("/appointments/{id}");
+
+        string[] methods = _sut.Definition.Verbs;
+        methods.Should().HaveCount(1).And.ContainSingle("PATCH");
+    }
+
+    [Fact]
+    public async Task Return_NoContent()
+    {
+        // Act
+        Func<Task> patch = async () => _ = await _sut.ExecuteAsync([], CancellationToken.None);
+
+        // Assert
+        await patch.Should().NotThrowAsync();
+
+    }
+}
