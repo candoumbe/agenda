b1e6ad8bcf1dd80a18383a6cc1ab6ae043e7cecc
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-08-29 19:44:01 +0200

    ci: add `PublishApi` target in `Build.cs` for publishing API image
    - Add `RegistryConfiguration` record for Docker registry configuration
    - Update `.nuke/build.schema.json` to include `PublishApi` target
    - Update `Directory.Packages.props` to include repository information
    - Update `Agenda.API.csproj` to enable packing and single file publish

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Candoumbe.Pipelines.Components;
 using Candoumbe.Pipelines.Components.Formatting;
 using Candoumbe.Pipelines.Components.GitHub;
@@ -12,58 +13,65 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Codecov;
+using Nuke.Common.Tools.Docker;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitHub;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities;
+using static Nuke.Common.Tools.Docker.DockerTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Serilog.Log;
 
 [GitHubActions(
-    "integration",
-    GitHubActionsImage.UbuntuLatest,
-    AutoGenerate = false,
-    FetchDepth = 0,
-    OnPushBranchesIgnore = [IHaveMainBranch.MainBranchName],
-    PublishArtifacts = true,
-    InvokedTargets = [nameof(Tests), nameof(IPushNugetPackages.Publish), nameof(IPack.Pack)],
-    CacheKeyFiles = ["global.json", "src/**/*.csproj"],
-    ImportSecrets =
-    [
-        nameof(IPushNugetPackages.NuGetApiKey),
-        nameof(IReportCoverage.CodecovToken),
-        nameof(IMutationTest.StrykerDashboardApiKey)
-    ],
-    OnPullRequestExcludePaths =
-    [
-        "docs/*",
-        "README.md",
-        "CHANGELOG.md",
-        "LICENSE"
-    ]
-)]
+                  "integration",
+                  GitHubActionsImage.UbuntuLatest,
+                  AutoGenerate = false,
+                  FetchDepth = 0,
+                  OnPushBranchesIgnore = [IHaveMainBranch.MainBranchName],
+                  PublishArtifacts = true,
+                  InvokedTargets = [nameof(Tests), nameof(IPushNugetPackages.Publish), nameof(IPack.Pack)],
+                  CacheKeyFiles = ["global.json", "src/**/*.csproj"],
+                  ImportSecrets =
+                  [
+                      nameof(IPushNugetPackages.NuGetApiKey),
+                      nameof(IReportCoverage.CodecovToken),
+                      nameof(IMutationTest.StrykerDashboardApiKey)
+                  ],
+                  OnPullRequestExcludePaths =
+                  [
+                      "docs/*",
+                      "README.md",
+                      "CHANGELOG.md",
+                      "LICENSE"
+                  ]
+              )]
 [GitHubActions(
-    "delivery",
-    GitHubActionsImage.UbuntuLatest,
-    FetchDepth = 0,
-    AutoGenerate = false,
-    OnPushBranches = [IHaveMainBranch.MainBranchName, IGitFlow.ReleaseBranch + "/*"],
-    InvokedTargets = [nameof(Tests), nameof(IPushNugetPackages.Publish), nameof(ICreateGithubRelease.AddGithubRelease)],
-    EnableGitHubToken = true,
-    CacheKeyFiles = ["global.json", "src/**/*.csproj"],
-    PublishArtifacts = true,
-    ImportSecrets =
-    [
-        nameof(IPushNugetPackages.NuGetApiKey),
-        nameof(IReportCoverage.CodecovToken),
-        nameof(IMutationTest.StrykerDashboardApiKey)
-    ],
-    OnPullRequestExcludePaths =
-    [
-        "docs/*",
-        "README.md",
-        "CHANGELOG.md",
-        "LICENSE"
-    ]
-)]
+                  "delivery",
+                  GitHubActionsImage.UbuntuLatest,
+                  FetchDepth = 0,
+                  AutoGenerate = false,
+                  OnPushBranches = [IHaveMainBranch.MainBranchName, IGitFlow.ReleaseBranch + "/*"],
+                  InvokedTargets = [nameof(Tests), nameof(IPushNugetPackages.Publish), nameof(ICreateGithubRelease.AddGithubRelease)],
+                  EnableGitHubToken = true,
+                  CacheKeyFiles = ["global.json", "src/**/*.csproj"],
+                  PublishArtifacts = true,
+                  ImportSecrets =
+                  [
+                      nameof(IPushNugetPackages.NuGetApiKey),
+                      nameof(IReportCoverage.CodecovToken),
+                      nameof(IMutationTest.StrykerDashboardApiKey)
+                  ],
+                  OnPullRequestExcludePaths =
+                  [
+                      "docs/*",
+                      "README.md",
+                      "CHANGELOG.md",
+                      "LICENSE"
+                  ]
+              )]
+[DotNetVerbosityMapping]
 public class Build : EnhancedNukeBuild,
+    IHaveGitVersion,
     IHaveSourceDirectory,
     IHaveTestDirectory,
     IGitFlowWithPullRequest,
@@ -77,11 +85,6 @@ public class Build : EnhancedNukeBuild,
     IPushNugetPackages,
     ICreateGithubRelease
 {
-    [Solution][Required] public readonly Solution Solution;
-
-    /// <inheritdoc />
-    Solution IHaveSolution.Solution => Solution;
-
     public static int Main() => Execute<Build>(x => ((ICompile)x).Compile);
 
     ///<inheritdoc/>
@@ -98,23 +101,23 @@ public class Build : EnhancedNukeBuild,
     AbsolutePath IHaveTestDirectory.TestDirectory => RootDirectory / "tests";
 
     ///<inheritdoc/>
-    IEnumerable<Project> IUnitTest.UnitTestsProjects => Solution.GetAllProjects("*.UnitTests");
+    IEnumerable<Project> IUnitTest.UnitTestsProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*.UnitTests");
 
     ///<inheritdoc/>
-    IEnumerable<Project> IIntegrationTest.IntegrationTestsProjects => Solution.GetAllProjects("*.IntegrationTests");
+    IEnumerable<Project> IIntegrationTest.IntegrationTestsProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*.IntegrationTests");
 
     private static readonly string[] s_projectWithUnitTests = ["Agenda.API", "Agenda.Ids", "Agenda.Objects"];
 
     ///<inheritdoc/>
     IEnumerable<MutationProjectConfiguration> IMutationTest.MutationTestsProjects
         => s_projectWithUnitTests
-            .Select(projectName => new MutationProjectConfiguration(sourceProject: Solution.AllProjects.Single(csproj => string.Equals(csproj.Name, projectName, StringComparison.InvariantCultureIgnoreCase)),
-                testProjects: Solution.AllProjects.Where(csproj => string.Equals(csproj.Name, $"{projectName}.UnitTests", StringComparison.InvariantCultureIgnoreCase)),
-                configurationFile: this.Get<IHaveTestDirectory>().TestDirectory / $"{projectName}.UnitTests" / "stryker-config.json"))
+            .Select(projectName => new MutationProjectConfiguration(sourceProject: this.Get<IHaveSolution>().Solution.AllProjects.Single(csproj => string.Equals(csproj.Name, projectName, StringComparison.InvariantCultureIgnoreCase)),
+                                                                    testProjects: this.Get<IHaveSolution>().Solution.AllProjects.Where(csproj => string.Equals(csproj.Name, $"{projectName}.UnitTests", StringComparison.InvariantCultureIgnoreCase)),
+                                                                    configurationFile: this.Get<IHaveTestDirectory>().TestDirectory / $"{projectName}.UnitTests" / "stryker-config.json"))
             .ToArray();
 
     ///<inheritdoc/>
-    IEnumerable<Project> IBenchmark.BenchmarkProjects => Solution.GetAllProjects("*.PerformanceTests");
+    IEnumerable<Project> IBenchmark.BenchmarkProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*.PerformanceTests");
 
     ///<inheritdoc/>
     bool IReportCoverage.ReportToCodeCov => this.Get<IReportCoverage>().CodecovToken is not null;
@@ -160,17 +163,16 @@ public class Build : EnhancedNukeBuild,
     /// <inheritdoc/>
     bool IDotnetFormat.VerifyNoChanges => IsServerBuild;
 
-    private IReadOnlyList<Project> ArchitecturalTestsProjects => [.. this.Get<IHaveSolution>().Solution.AllProjects.Where(project => project.Name.Like("*.ArchitecturalTests", ignoreCase: true)) ];
+    private IReadOnlyList<Project> ArchitecturalTestsProjects => [.. this.Get<IHaveSolution>().Solution.AllProjects.Where(project => project.Name.Like("*.ArchitecturalTests", ignoreCase: true))];
 
     /// <summary>
     /// Target to run architectural tests.
     /// </summary>
-    public Target ArchitecturalTests => _ =>  _.TryTriggeredBy<IUnitTest>() // <- This will make architectural tests run whenever unit tests run
+    public Target ArchitecturalTests => _ => _.TryTriggeredBy<IUnitTest>() // <- This will make architectural tests run whenever unit tests run
                                             .TryBefore<IMutationTest>()
                                             .TryDependsOn<ICompile>()
                                             .Description("Runs architectural tests")
                                             .Executes(() =>
-
                                                           DotNetTest(s => s.SetConfiguration(this.Get<IHaveConfiguration>().Configuration)
                                                                          .SetNoBuild(SucceededTargets.Contains(this.Get<ICompile>().Compile))
                                                                          .SetNoRestore(SucceededTargets.Contains(this.Get<IRestore>().Restore))
@@ -180,6 +182,53 @@ public class Build : EnhancedNukeBuild,
                                                                                                        (x, framework) => x.SetFramework(framework)))
                                                                     )
                                                      );
+
+    private AbsolutePath ApiProject => this.Get<IHaveSourceDirectory>().SourceDirectory / "Agenda.API";
+
+    internal IReadOnlyList<RegistryConfiguration> Registries =>
+    [
+        new RegistryConfiguration("ghcr.io",
+                                  this.Get<IHaveGitHubRepository>().GitRepository.GetGitHubOwner(),
+                                  this.Get<IHaveGitHubRepository>().GitHubToken)
+    ];
+
+    public Target PublishApi => _ => _.Inherit<IPack>()
+                                    .Description("Publish image of the API")
+                                    .After(Tests)
+                                    .Produces(this.Get<IHaveArtifacts>().ArtifactsDirectory / "publish" / "agenda.api-*.tar.gz")
+                                    .Executes(() =>
+                                              {
+                                                  GitVersion gitVersion = this.Get<IHaveGitVersion>().GitVersion;
+                                                  string version = gitVersion.FullSemVer;
+                                                  string imageName = "agenda.api";
+                                                  string filename = $"{imageName}-{version}.tar.gz";
+                                                  Project project = this.Get<IHaveSolution>().Solution.AllProjects.Single(project => project.Name == "Agenda.API");
+                                                  AbsolutePath containerFullPath = this.Get<IHaveArtifacts>().ArtifactsDirectory / "publish" / filename;
+                                                  Information("Publishing {ImageName} (version {Version}) to {ContainerFullPath}", project.Name, version, containerFullPath);
+
+                                                  if (IsServerBuild)
+                                                  {
+                                                      DockerLogin(loginConfig => loginConfig.SetUsername(this.Get<IHaveGitRepository>().GitRepository.GetGitHubOwner())
+                                                                      .SetPassword(this.Get<IHaveGitHubRepository>().GitHubToken));
+                                                  }
+
+                                                  DotNetPublish(settings => settings.SetProject(project)
+                                                                    .SetConfiguration(this.Get<IHaveConfiguration>().Configuration)
+                                                                    // .SetNoRestore(InvokedTargets.Contains(this.Get<IRestore>().Restore) && SucceededTargets.Contains(this.Get<IRestore>().Restore))
+                                                                    // .SetNoBuild(InvokedTargets.Contains(this.Get<ICompile>().Compile) && SucceededTargets.Contains(this.Get<ICompile>().Compile))
+                                                                    .EnableSelfContained()
+                                                                    .When(IsServerBuild, target => target.SetProperty("ContainerRegistry", "ghcr.io"))
+                                                                    .SetProperties(new Dictionary<string, object>
+                                                                    {
+                                                                        ["ContainerArchiveOutputPath"] = containerFullPath,
+                                                                        ["ContainerImageName"] = imageName,
+                                                                        ["ContainerImageTag"] = gitVersion.SemVer,
+                                                                        //["PublishRepositoryUrl"] = true,
+                                                                        ["ContainerGenerateLabelsImageCreated"] = DateTime.UtcNow.ToString("O")
+                                                                    })
+                                                                    .SetProcessAdditionalArguments("/t:PublishContainer"));
+                                              });
+
 
     public Target Tests => _ => _.Triggers(ArchitecturalTests,
                                            this.Get<IUnitTest>().UnitTests,
---
+++ build/RegistryConfiguration.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// Registry configuration
+/// </summary>
+/// <param name="Registry"></param>
+/// <param name="Username"></param>
+/// <param name="Password"></param>
+public record RegistryConfiguration(string Registry, string Username, string Password);
