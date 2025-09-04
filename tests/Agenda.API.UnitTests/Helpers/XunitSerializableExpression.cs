ebbd7dba93a20976b5ce418ca77d1bf107fea29b
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-09-04 22:17:17 +0200

    fix(tests): fix running unit tests through CLI
    - replace `Remote.Linq` dependency with `Serialize.Linq`
    - update XunitSerializableExpression implementation to use `Serialize.Linq`

--- tests/Agenda.API.UnitTests/Helpers/XunitSerializableExpression.cs
+++ tests/Agenda.API.UnitTests/Helpers/XunitSerializableExpression.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Text.Json;
 using Fluxera.StronglyTypedId.SystemTextJson;
 using NodaTime;
 using NodaTime.Serialization.SystemTextJson;
-using Remote.Linq;
-using Remote.Linq.Text.Json;
+using Serialize.Linq.Serializers;
 using Xunit.Sdk;
+using JsonSerializer = Serialize.Linq.Serializers.JsonSerializer;
 
 namespace Agenda.API.UnitTests.Helpers;
 
@@ -15,26 +14,17 @@ public class XunitSerializableExpression<T>: IXunitSerializable
 {
     public Expression<Func<T, bool>> Value { get; set; }
 
-    private readonly JsonSerializerOptions _serializerSettings;
-    private readonly ExpressionTranslatorContext _expressionTranslatorContext;
+    private readonly ExpressionSerializer _serializer = new(new JsonSerializer());
 
-    public XunitSerializableExpression()
-    {
 
-        _serializerSettings = new JsonSerializerOptions().ConfigureRemoteLinq();
-        _serializerSettings.UseStronglyTypedId();
-        _serializerSettings.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
-        _expressionTranslatorContext = new ExpressionTranslatorContext(valueMapper: new KnownDynamicObjectMapper());
-    }
-
+    /// <inheritdoc />
     public void Deserialize(IXunitSerializationInfo info)
     {
-        Console.WriteLine(info.GetValue<string>(nameof(Value)));
-        Value = (Expression<Func<T, bool>>) JsonSerializer.Deserialize<Remote.Linq.Expressions.LambdaExpression>(info.GetValue<string>(nameof(Value)), _serializerSettings).ToLinqExpression(_expressionTranslatorContext)!;
+        Value = _serializer.DeserializeText(info.GetValue<string>(nameof(Value))) as Expression<Func<T, bool>>;
     }
 
-    public void Serialize(IXunitSerializationInfo info) =>
-        info.AddValue(nameof(Value), JsonSerializer.Serialize(Value.ToRemoteLinqExpression(_expressionTranslatorContext), _serializerSettings));
+    /// <inheritdoc />
+    public void Serialize(IXunitSerializationInfo info) => info.AddValue(nameof(Value), _serializer.SerializeText(Value));
 
 
     public static implicit operator Expression<Func<T, bool>>(XunitSerializableExpression<T> expressionSerializable) => expressionSerializable.Value;
