3dd04554d07577d42e243f98f4d66ebabe470a82
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-09-29 21:02:29 +0200

    chore: add [ExcludeFromCodeCoverage] to InvalidEmailException class
    Signed-off-by: Cyrille NDOUMBE <ndoumbecyrille@gmail.com>

--- src/Agenda.Objects/InvalidEmailException.cs
+++ src/Agenda.Objects/InvalidEmailException.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Agenda.Objects;
 
 /// <summary>
 /// Exception thrown when an email is invalid.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public class InvalidEmailException : Exception
 {
     /// <inheritdoc />
