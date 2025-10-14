7b61630d0c9170f2bdbb6778e52b2b3c1da9563a
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-10-14 20:59:57 +0200

    feat(api): emit `AppointmentScheduled` event after creating appointment
    - Add `Agenda.Events` project reference to `Agenda.API`
    - Inject `IAmACommandProcessor` to handle event publishing
    - Update `CreateAppointmentEndpoint` to emit `AppointmentScheduled` event upon appointment creation
    - Enhance unit tests to verify event publishing logic
    
    Signed-off-by: Cyrille NDOUMBE <ndoumbecyrille@gmail.com>

--- src/Agenda.API/Features/Appointments/v1/Create/CreateAppointmentEndpoint.cs
+++ src/Agenda.API/Features/Appointments/v1/Create/CreateAppointmentEndpoint.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Agenda.API.Features.Appointments.v1.Delete;
 using Agenda.API.Features.Appointments.v1.GetById;
 using Agenda.API.Features.v1.Appointments;
+using Agenda.Events;
 using Agenda.Objects;
 using Candoumbe.DataAccess.Abstractions;
 using Candoumbe.Forms;
@@ -12,6 +13,8 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using NodaTime;
+using Paramore.Brighter;
+using Attendee = Agenda.Objects.Attendee;
 
 namespace Agenda.API.Features.Appointments.v1.Create;
 
@@ -23,6 +26,7 @@ public class CreateAppointmentEndpoint : Endpoint<NewAppointmentInfo, CreatedAtR
     private readonly IUnitOfWorkFactory _unitOfWorkFactory;
     private readonly LinkGenerator _linkGenerator;
     private readonly CurrentRequestMetadataInfoProvider _currentRequestMetadataInfoProvider;
+    private readonly IAmACommandProcessor _commandProcessor;
 
 
     /// <summary>
@@ -31,11 +35,13 @@ public class CreateAppointmentEndpoint : Endpoint<NewAppointmentInfo, CreatedAtR
     /// <param name="unitOfWorkFactory"></param>
     /// <param name="linkGenerator"></param>
     /// <param name="currentRequestMetadataInfoProvider"></param>
-    public CreateAppointmentEndpoint(IUnitOfWorkFactory unitOfWorkFactory, LinkGenerator linkGenerator, CurrentRequestMetadataInfoProvider currentRequestMetadataInfoProvider)
+    /// <param name="commandProcessor"></param>
+    public CreateAppointmentEndpoint(IUnitOfWorkFactory unitOfWorkFactory, LinkGenerator linkGenerator, CurrentRequestMetadataInfoProvider currentRequestMetadataInfoProvider, IAmACommandProcessor commandProcessor)
     {
         _unitOfWorkFactory = unitOfWorkFactory;
         _linkGenerator = linkGenerator;
         _currentRequestMetadataInfoProvider = currentRequestMetadataInfoProvider;
+        _commandProcessor = commandProcessor;
     }
 
     /// <inheritdoc />
@@ -62,6 +68,20 @@ public class CreateAppointmentEndpoint : Endpoint<NewAppointmentInfo, CreatedAtR
         }
 
         await unitOfWork.Repository<Appointment>().Create(newAppointment, ct);
+
+        AppointmentScheduled appointmentScheduledEvent = new(newAppointment.Id,
+                                                             newAppointment.Subject,
+                                                             newAppointment.Location,
+                                                             [
+                                                                 ..newAppointment.Attendees.Select(a => new Agenda.Events.Attendee()
+                                                                            {
+                                                                                Id = a.Id.Value,
+                                                                                FirstName = a.Name,
+                                                                                LastName = a.Email
+                                                                            })
+                                                             ]);
+
+        await _commandProcessor.DepositPostAsync(appointmentScheduledEvent, cancellationToken: ct);
         await unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
 
         DateTimeZone zone = _currentRequestMetadataInfoProvider.GetCurrentDateTimeZone();
