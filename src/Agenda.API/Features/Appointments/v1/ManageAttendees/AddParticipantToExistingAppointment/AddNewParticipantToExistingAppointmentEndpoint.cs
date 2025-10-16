0b9566f47754cbf2e2489310e62bbcc91c2babaa
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-10-16 18:15:24 +0200

    feat(api): add endpoint to add participant to existing appointment
    - introduce `POST /appointments/{id}/attendees` endpoint to allow adding participants to existing appointments
    - added associated request model and unit tests
    
    Signed-off-by: Cyrille NDOUMBE <ndoumbecyrille@gmail.com>

---
+++ src/Agenda.API/Features/Appointments/v1/ManageAttendees/AddParticipantToExistingAppointment/AddNewParticipantToExistingAppointmentEndpoint.cs
@@ -0,0 +1,61 @@
+using Agenda.Objects;
+using Candoumbe.DataAccess.Abstractions;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Optional;
+
+namespace Agenda.API.Features.Appointments.v1.ManageAttendees.AddParticipantToExistingAppointment;
+
+/// <summary>
+/// Remove an attendee from an appointment
+/// </summary>
+public class AddNewParticipantToExistingAppointmentEndpoint : Endpoint<AddNewParticipantToExistingAppointmentRequest, Results<NoContent, Conflict, NotFound>>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+    /// <summary>
+    /// Builds a new <see cref="AddNewParticipantToExistingAppointmentEndpoint"/> instance.
+    /// </summary>
+    /// <param name="unitOfWorkFactory"></param>
+    public AddNewParticipantToExistingAppointmentEndpoint(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    /// <inheritdoc />
+    public override void Configure()
+    {
+        Post("/appointments/{id}/attendees");
+    }
+
+    /// <inheritdoc />
+    public override async Task<Results<NoContent, Conflict, NotFound>> ExecuteAsync(AddNewParticipantToExistingAppointmentRequest req, CancellationToken ct)
+    {
+
+        using IUnitOfWork unitOfWork = _unitOfWorkFactory.NewUnitOfWork();
+
+        IFilterSpecification<Appointment> predicate = new FilterSpecification<Appointment>(a => a.Id == req.Id);
+        Option<Appointment> maybeAppointment = await unitOfWork.Repository<Appointment>().SingleOrDefault(predicate, ct);
+        return await maybeAppointment.Match(
+            some: async (existingAppointment) =>
+            {
+                Results<NoContent, Conflict, NotFound> result;
+                if (existingAppointment.Attendees.All(a => a.Id != req.Participant.Id))
+                {
+                    existingAppointment.RemoveAttendee(req.Participant.Id);
+                    await unitOfWork.SaveChangesAsync(ct);
+
+                    result = TypedResults.NoContent();
+                }
+                else
+                {
+                    result = TypedResults.Conflict();
+                }
+
+                return result;
+            },
+            none: () => Task.FromResult<Results<NoContent,Conflict, NotFound>>(TypedResults.NotFound())
+        );
+        return TypedResults.NotFound();
+    }
+}
