6e19cd6f6f540f28f5df7330790366334a21db93
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-12-15 11:20:57 +0100

    feat(db): add migration for `outbox` table
    - Implement `AddOutboxTable` migration using `PostgreSqlOutboxBuilder`
    - Generate designer file for migration
    
    Signed-off-by: Cyrille NDOUMBE <ndoumbecyrille@gmail.com>

---
+++ src/Agenda.DataStores.Postgres/Migrations/20251215101906_AddOutboxTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Paramore.Brighter.Outbox.PostgreSql;
+
+#nullable disable
+
+namespace Agenda.DataStores.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOutboxTable : Migration
+    {
+        private const string OutboxTableName = "outbox";
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string ddl = PostgreSqlOutboxBuilder.GetDDL(OutboxTableName);
+            migrationBuilder.Sql(ddl);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(OutboxTableName);
+        }
+    }
+}
