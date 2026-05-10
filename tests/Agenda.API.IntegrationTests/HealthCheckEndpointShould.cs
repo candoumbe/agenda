96e0e6e741b261ed83d4bb4b9a9c323e0e311583
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2026-05-10 12:08:45 +0000

    test(integration): stabilize healthcheck startup flow

--- tests/Agenda.API.IntegrationTests/HealthCheckEndpointShould.cs
+++ tests/Agenda.API.IntegrationTests/HealthCheckEndpointShould.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -11,22 +12,28 @@ using Xunit.OpenCategories.V3;
 
 namespace Agenda.API.IntegrationTests;
 
-[IntegrationTests]
+[IntegrationTest]
 public class HealthCheckEndpointShould(ITestOutputHelper outputHelper) : IAsyncLifetime
 {
     private HttpClient _client;
     private AgendaApplicationTestingBuilder _appHost;
-
+    private static readonly TimeSpan s_buildStopTimeout = TimeSpan.FromSeconds(60);
+    
     /// <inheritdoc />
     public async ValueTask InitializeAsync()
     {
-        _appHost = await DistributedApplicationTestingBuilderFactory.CreateBuilderAsync(outputHelper);
+        _appHost = await DistributedApplicationTestingBuilderFactory.CreateBuilderAsync(outputHelper, TestContext.Current.CancellationToken);
         await _appHost.StartAsync(TestContext.Current.CancellationToken);
         _client = _appHost.ApiClient;
     }
 
     /// <inheritdoc />
-    public async ValueTask DisposeAsync() => await _appHost.DisposeAsync();
+    public async ValueTask DisposeAsync()
+    {
+        // ReSharper disable DisposeOnUsingVariable
+        await _appHost.DisposeAsync();
+        // ReSharper restore DisposeOnUsingVariable
+    }
 
     [RetryFact(maxRetries: 3, delayBetweenRetriesMs: 2000, SkipExceptions = [typeof(DistributedApplicationException)])]
     public async Task Return_healthy_status_from_health_endpoint()
