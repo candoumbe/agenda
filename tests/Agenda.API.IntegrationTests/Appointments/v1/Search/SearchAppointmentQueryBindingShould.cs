729fb9bf895c4949610efa853acea5000ffd4459
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2026-05-24 11:51:56 +0000

    test(integration): align transient retry constants naming

--- tests/Agenda.API.IntegrationTests/Appointments/v1/Search/SearchAppointmentQueryBindingShould.cs
+++ tests/Agenda.API.IntegrationTests/Appointments/v1/Search/SearchAppointmentQueryBindingShould.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Agenda.API.IntegrationTests.Fixtures;
