d7340129e8e05ba88a723c0968d2b1a7688a3258
Cyrille NDOUMBE <candoumbe@users.noreply.github.com>
2026-06-28 21:52:42 +0000

    chore: update build configurations (#624)
    ## Summary
    - update build and pipeline configurations after migration from Nuke to
    Fallout
    - include related tooling updates on the `chore/update-ci-to-3-x` branch
    
    ## Validation
    - branch pushed and ready for review

--- build/Build.cs
+++ build/Build.cs
@@ -7,26 +7,26 @@ using Candoumbe.Pipelines.Components.Formatting;
 using Candoumbe.Pipelines.Components.GitHub;
 using Candoumbe.Pipelines.Components.NuGet;
 using Candoumbe.Pipelines.Components.Workflows;
-using Nuke.Common;
-using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Git;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Tools.Codecov;
-using Nuke.Common.Tools.Docker;
-using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.EntityFramework;
-using Nuke.Common.Tools.GitHub;
-using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.Npm;
-using static Nuke.Common.Tools.Docker.DockerTasks;
-using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tools.EntityFramework.EntityFrameworkTasks;
-using static Nuke.Common.Tools.Npm.NpmTasks;
-using static Nuke.Common.Utilities.ConsoleUtility;
+using Fallout.Common;
+using Fallout.Common.CI.GitHubActions;
+using Fallout.Common.Git;
+using Fallout.Common.IO;
+using Fallout.Common.ProjectModel;
+using Fallout.Common.Tooling;
+using Fallout.Common.Tools.Codecov;
+using Fallout.Common.Tools.Docker;
+using Fallout.Common.Tools.DotNet;
+using Fallout.Common.Tools.EntityFramework;
+using Fallout.Common.Tools.GitHub;
+using Fallout.Common.Tools.GitVersion;
+using Fallout.Common.Tools.Npm;
+using static Fallout.Common.Tools.Docker.DockerTasks;
+using static Fallout.Common.Tools.DotNet.DotNetTasks;
+using static Fallout.Common.Tools.EntityFramework.EntityFrameworkTasks;
+using static Fallout.Common.Tools.Npm.NpmTasks;
+using static Fallout.Common.Utilities.ConsoleUtility;
 using static Serilog.Log;
-using Project = Nuke.Common.ProjectModel.Project;
+using Project = Fallout.Common.ProjectModel.Project;
 
 [GitHubActions(
     "integration",
@@ -45,13 +45,6 @@ using Project = Nuke.Common.ProjectModel.Project;
         nameof(IMutationTest.StrykerDashboardApiKey)
     ],
     OnPushExcludePaths =
-    [
-        "docs/**",
-        "README.md",
-        "CHANGELOG.md",
-        "LICENSE"
-    ],
-    OnPullRequestExcludePaths =
     [
         "docs/**",
         "README.md",
@@ -81,17 +74,10 @@ using Project = Nuke.Common.ProjectModel.Project;
         "README.md",
         "CHANGELOG.md",
         "LICENSE"
-    ],
-    OnPullRequestExcludePaths =
-    [
-        "docs/**",
-        "README.md",
-        "CHANGELOG.md",
-        "LICENSE"
     ]
 )]
 [DotNetVerbosityMapping]
-public class Build : EnhancedNukeBuild,
+public class Build : EnhancedBuild,
     IHaveGitVersion,
     IHaveSourceDirectory,
     IHaveTestDirectory,
@@ -140,13 +126,13 @@ public class Build : EnhancedNukeBuild,
     Configure<DotNetTestSettings, (Project project, string framework)> IUnitTest.ProjectUnitTestSettings => (settings, unitTestRunContext) => settings
         .ResetProjectFile()
         .ClearLoggers()
-        .SetProcessAdditionalArguments($"--project {unitTestRunContext.project}");
+        .SetProcessAdditionalArguments($"--project {unitTestRunContext.project.Path}");
 
     ///<inheritdoc/>
     Configure<DotNetTestSettings, (Project project, string framework)> IIntegrationTest.ProjectIntegrationTestSettings => (settings, testRunContext) => settings
         .ResetProjectFile()
         .ClearLoggers()
-        .SetProcessAdditionalArguments($"--project {testRunContext.project}");
+        .SetProcessAdditionalArguments($"--project {testRunContext.project.Path}");
 
     ///<inheritdoc/>
     IEnumerable<Project> IIntegrationTest.IntegrationTestsProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*.IntegrationTests");
@@ -217,7 +203,7 @@ public class Build : EnhancedNukeBuild,
                                                                          .SetNoBuild(SucceededTargets.Contains(this.Get<ICompile>().Compile))
                                                                          .SetNoRestore(SucceededTargets.Contains(this.Get<IRestore>().Restore))
                                                                          .CombineWith(ArchitecturalTestsProjects,
-                                                                                      (setting, project) => setting.SetProcessAdditionalArguments($"--project {project}")
+                                                                                      (setting, project) => setting.SetProcessAdditionalArguments($"--project {project.Path}")
                                                                                           .CombineWith(project.GetTargetFrameworks(),
                                                                                                        (x, framework) => x.SetFramework(framework)))
                                                                     )
