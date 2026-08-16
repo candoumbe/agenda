3873e2963671dfbb22254115992cfee5cf60b2a4
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2026-08-16 10:28:04 +0000

    style(agenda): modernize C# namespace declarations

--- src/Agenda.API/Features/Appointments/v1/ManageAttendees/RemoveAttendee/RemoveAttendeeRequest.cs
+++ src/Agenda.API/Features/Appointments/v1/ManageAttendees/RemoveAttendee/RemoveAttendeeRequest.cs
@@ -1,17 +1,19 @@
 using Agenda.Ids;
 
-namespace Agenda.API.Features.Appointments.v1.ManageAttendees.RemoveAttendee
-{
-    public record RemoveAttendeeRequest
-    {
-        /// <summary>
-        /// Identifier of the attendee to remove.
-        /// </summary>
-        public required AttendeeId AttendeeId { get; init; }
+namespace Agenda.API.Features.Appointments.v1.ManageAttendees.RemoveAttendee;
 
-        /// <summary>
-        /// Identifier of the appointment from which to remove the attendee.
-        /// </summary>
-        public required AppointmentId Id { get; init; }
-    }
+/// <summary>
+/// Request to remove an attendee from an appointment
+/// </summary>
+public record RemoveAttendeeRequest
+{
+    /// <summary>
+    /// Identifier of the attendee to remove.
+    /// </summary>
+    public required AttendeeId AttendeeId { get; init; }
+
+    /// <summary>
+    /// Identifier of the appointment from which to remove the attendee.
+    /// </summary>
+    public required AppointmentId Id { get; init; }
 }
